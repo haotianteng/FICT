c8dfb9eb7287a159fb8eea69e16a4def221bd398
HaotianTeng <havens.teng@gmail.com>
2020-09-01 05:54:24 -0400

    Reshape back field argument

--- fict/fict_input.py
+++ fict/fict_input.py
@@ -34,11 +34,13 @@ class RealDataLoader(dop.DataLoader):
         self.class_n = num_class
         if field is None:
             field = np.zeros(self.sample_n)
+        field = np.asarray(field)
         self.field = field.reshape((field.shape[0],1))
         if cell_coordinate.shape[1] == 2:
             self.coordinate = np.concatenate((cell_coordinate,1000*self.field),axis=1)
         else:
             self.coordinate = cell_coordinate
+        self.field = field.reshape((field.shape[0]))
         self.adjacency = dop.get_adjacency(self.coordinate,threshold_distance)
         self.exclude_adjacency = dop.get_adjacency(self.coordinate,threshold_distance,exclude_self = True)
         self.for_eval = for_eval
@@ -98,47 +100,4 @@ class RealDataLoader(dop.DataLoader):
             self.reduced_gene_expression,_ = dop.embedding_reduce(self.gene_expression,embedding = embedding)
             self.xs = (self.reduced_gene_expression,self.nb_count)
 if __name__ == "__main__":
-    ### Hyper parameter setting
-    print("Setting hyper parameter")
-    sample_n = 1000 #Number of samples
-    n_g = 15 #Number of genes
-    n_c = 10 #Number of cell type
-    density = 20 #The average number of neighbour for each cells.
-    threshold_distance = 100 # The threshold distance of neighbourhood.
-    gene_col = np.arange(9,164)
-    coor_col = [5,6]
-    header = 0
-    data_f = "/home/heavens/CMU/FISH_Clustering/MERFISH2018/Moffitt_and_Bambah-Mukku_et_al_merfish_all_cells.csv"
-    save_f = "/home/heavens/CMU/FISH_Clustering/MERFISH_data/df_"
-    ### Data preprocessing
-    print("Reading data from %s"%(data_f))
-    if data_f.endswith('.xlsx'):
-        data_all = pd.read_excel(data_f,header = header)
-    elif data_f.endswith('.csv'):
-        data_all = pd.read_csv(data_f,header = header)
-    animal_idxs = np.unique(data_all['Animal_ID'])
-    gene_expression_all = data_all.iloc[:,gene_col]
-    nan_cols = np.unique(np.where(np.isnan(gene_expression_all))[1])
-    for nan_col in nan_cols:
-        gene_col = np.delete(gene_col,nan_col)
-    for animal_id in animal_idxs:
-        print("Extract the data for animal %d"%(animal_id))
-        data = data_all[data_all['Animal_ID']==animal_id]
-        cell_types = data['Cell_class']
-        data = data[cell_types!= 'Ambiguous']
-        cell_types = data['Cell_class']
-        bregma = data['Bregma']
-        gene_expression = data.iloc[:,gene_col]
-        type_tags = np.unique(cell_types)
-        coordinates = data.iloc[:,coor_col]
-        coordinates = np.asarray(coordinates)
-        gene_expression = np.asarray(gene_expression)
-        gene_expression = gene_expression/np.sum(gene_expression,axis = 1,keepdims = True)
-        real_df = RealDataLoader(gene_expression,
-                                 coordinates,
-                                 threshold_distance = threshold_distance,
-                                 cell_labels = cell_types,
-                                 num_class = n_c,
-                                 field = bregma,
-                                 for_eval = False)
-        dop.save_loader(real_df,save_f+str(animal_id))
+    pass
