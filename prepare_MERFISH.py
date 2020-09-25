3ae279ce13dfe5be7b05860fe5e7429b93fbe54d
HaotianTeng <havens.teng@gmail.com>
2020-09-25 13:28:40 -0400

    Use 1-start index instead of 0-index to save smfishHmrf dataset

--- prepare_MERFISH.py
+++ prepare_MERFISH.py
@@ -22,6 +22,7 @@ coor_col = [5,6]
 header = 0
 data_f = "/home/heavens/CMU/FISH_Clustering/MERFISH2018/Moffitt_and_Bambah-Mukku_et_al_merfish_all_cells.csv"
 save_f = "/home/heavens/CMU/FISH_Clustering/MERFISH_data/df_"
+smfishHmrf_save = "/home/heavens/CMU/FISH_Clustering/Benchmark/Merfish/data/"
 ### Data preprocessing
 print("Reading data from %s"%(data_f))
 if data_f.endswith('.xlsx'):
@@ -33,6 +34,8 @@ gene_expression_all = data_all.iloc[:,gene_col]
 nan_cols = np.unique(np.where(np.isnan(gene_expression_all))[1])
 for nan_col in nan_cols:
     gene_col = np.delete(gene_col,nan_col)
+gene_name = data_all.columns[gene_col]
+bregmas_smfish = [0.01]*3 + [-0.04]+[0.01]*3+[-0.04]*4+[0.11]*25
 for animal_id in animal_idxs:
     print("Extract the data for animal %d"%(animal_id))
     data = data_all[data_all['Animal_ID']==animal_id]
@@ -49,11 +52,23 @@ for animal_id in animal_idxs:
     coordinates = np.asarray(coordinates)
     gene_expression = np.asarray(gene_expression)
     gene_expression = gene_expression/np.sum(gene_expression,axis = 1,keepdims = True)
+    gene_names = data
     real_df = RealDataLoader(gene_expression,
                              coordinates,
                              threshold_distance = threshold_distance,
+                             gene_list = gene_name,
                              cell_labels = cell_types,
                              num_class = n_c,
                              field = bregma,
                              for_eval = False)
+    mask = bregma == bregmas_smfish[animal_id-1]
+    smfish_loader = RealDataLoader(gene_expression[mask,:],
+								   coordinates[mask,:],
+								   threshold_distance = threshold_distance,
+                                   gene_list = gene_name,
+                                   cell_labels = cell_types[mask],
+                                   num_class = n_c,
+                                   field = [bregmas_smfish[animal_id-1]]*sum(mask),
+                                   for_eval = False)
+    dop.save_smfish(smfish_loader,smfishHmrf_save+str(animal_id))
     dop.save_loader(real_df,save_f+str(animal_id))
